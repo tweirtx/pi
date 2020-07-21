a3bbf69b71a9a3b6061291f4c60c13403ea17e25
Travis Weir <tweirtx@gmail.com>
2020-07-21 02:56:22 -0500

    Redo everything *properly*

---
+++ stage2/04-install-streamline/01-run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cat /etc/apt/sources.list
+
+apt update
+
+apt install gst-rpicamsrc rusty-engine gstreamer1.0-tools -y --allow-unauthenticated
+
+echo "start_x=1             # essential
+gpu_mem=128           # at least, or maybe more if you wish" >> /boot/config.txt
--- stage2/prerun.sh
+++ stage2/prerun.sh
@@ -3,12 +3,3 @@
 if [ ! -d "${ROOTFS_DIR}" ]; then
 	copy_previous
 fi
-
-echo "deb [allow-insecure=yes trusted=yes] http://dl.bintray.com/tweirtx/streamline stretch main" >> /etc/apt/sources.list
-
-apt update
-
-apt install gst-rpicamsrc rusty-engine gstreamer1.0-tools -y --allow-unauthenticated
-
-echo "start_x=1             # essential
-gpu_mem=128           # at least, or maybe more if you wish" >> /boot/config.txt
