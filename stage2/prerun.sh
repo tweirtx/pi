80d515d15a0f4c29cc9d80e89d11a48adcb1a568
Travis Weir <tweirtx@gmail.com>
2020-07-20 22:03:04 -0500

    Install deps for cam, get rid of NOOBS step

--- stage2/prerun.sh
+++ stage2/prerun.sh
@@ -3,6 +3,8 @@
 if [ ! -d "${ROOTFS_DIR}" ]; then
 	copy_previous
 fi
-echo "deb [allow-insecure=yes] https://dl.bintray.com/tweirtx/streamline stretch main" | sudo tee -a /etc/apt/sources.list
+echo "deb [allow-insecure=yes] https://dl.bintray.com/tweirtx/streamline stretch main" | tee -a /etc/apt/sources.list
 
 apt update
+
+apt install gst-rpicamsrc rusty-engine -y
