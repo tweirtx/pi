598f41b9228293805a9b9191ac93c19f9dd79afe
Travis Weir <tweirtx@gmail.com>
2020-07-20 22:20:44 -0500

    No sudo

--- stage2/prerun.sh
+++ stage2/prerun.sh
@@ -3,6 +3,6 @@
 if [ ! -d "${ROOTFS_DIR}" ]; then
 	copy_previous
 fi
-echo "deb [allow-insecure=yes] https://dl.bintray.com/tweirtx/streamline stretch main" | sudo tee -a /etc/apt/sources.list
+echo "deb [allow-insecure=yes] https://dl.bintray.com/tweirtx/streamline stretch main" | tee -a /etc/apt/sources.list
 
 apt update
