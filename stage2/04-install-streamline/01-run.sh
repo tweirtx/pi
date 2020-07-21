856048adac9571fcb320c63911a5f34c228d7e9a
Travis Weir <tweirtx@gmail.com>
2020-07-21 04:05:18 -0500

    Add service creation for rusty-engine

--- stage2/04-install-streamline/01-run.sh
+++ stage2/04-install-streamline/01-run.sh
@@ -8,3 +8,10 @@ apt install gst-rpicamsrc rusty-engine gstreamer1.0-tools -y --allow-unauthentic
 
 echo "start_x=1             # essential
 gpu_mem=128           # at least, or maybe more if you wish" >> /boot/config.txt
+
+echo "[Unit]
+Description=Expose RPi Cam over RTSP
+After=network-online.target
+
+[Service]
+ExecStart=rusty-engine -d /dev/video0 -h 720 -w 1280 -i rpi" > /etc/systemd/system/rusty.service
