d87336e69c77dab039a89367ca70c8ca8c5f0920
Travis Weir <tweirtx@gmail.com>
2020-07-21 03:25:19 -0500

    Move JDK install to correct script

--- stage2/prerun.sh
+++ stage2/prerun.sh
@@ -3,8 +3,3 @@
 if [ ! -d "${ROOTFS_DIR}" ]; then
 	copy_previous
 fi
-echo "deb [allow-insecure=yes] https://dl.bintray.com/tweirtx/streamline stretch main" | tee -a /etc/apt/sources.list
-
-apt update
-
-apt install default-jdk -y
