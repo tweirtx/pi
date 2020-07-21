ed3585711bbe3a0b6789216b738e1fba6aa2da9d
Travis Weir <tweirtx@gmail.com>
2020-07-21 01:04:09 -0500

    Fix apt error, enable camera

--- stage2/prerun.sh
+++ stage2/prerun.sh
@@ -3,8 +3,10 @@
 if [ ! -d "${ROOTFS_DIR}" ]; then
 	copy_previous
 fi
-echo "deb [allow-insecure=yes] https://dl.bintray.com/tweirtx/streamline stretch main" | tee -a /etc/apt/sources.list
 
 apt update
 
 apt install gst-rpicamsrc rusty-engine gstreamer1.0-tools -y --allow-unauthenticated
+
+echo "start_x=1             # essential
+gpu_mem=128           # at least, or maybe more if you wish" >> /boot/config.txt
