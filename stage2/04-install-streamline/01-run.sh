0f47704d987e26f413fec02d3062dac229150543
Travis Weir <tweirtx@gmail.com>
2020-07-21 04:31:59 -0500

    Revert "Yet another workaround"
    This reverts commit ef136d5cc54f66762993fa01fa40ba49f7967367.

--- stage2/04-install-streamline/01-run.sh
+++ stage2/04-install-streamline/01-run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-echo "deb [allow-insecure=yes] http://dl.bintray.com/tweirtx/streamline stretch main" >> /etc/apt/sources.list
 cat /etc/apt/sources.list
 
 apt update
 
-apt install gst-rpicamsrc rusty-engine gstreamer1.0-tools -y --allow-unauthenticated
+# apt install gst-rpicamsrc rusty-engine gstreamer1.0-tools -y --allow-unauthenticated
 
 echo "start_x=1             # essential
 gpu_mem=128           # at least, or maybe more if you wish" >> /boot/config.txt
