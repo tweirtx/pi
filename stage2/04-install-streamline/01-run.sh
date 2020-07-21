1787a2d79f546af353640e89246a1c4ca1258159
Travis Weir <tweirtx@gmail.com>
2020-07-21 03:36:34 -0500

    Another way of installing things

--- stage2/04-install-streamline/01-run.sh
+++ stage2/04-install-streamline/01-run.sh
@@ -3,7 +3,7 @@ cat /etc/apt/sources.list
 
 apt update
 
-apt install gst-rpicamsrc rusty-engine gstreamer1.0-tools -y --allow-unauthenticated
+# apt install gst-rpicamsrc rusty-engine gstreamer1.0-tools -y --allow-unauthenticated
 
 echo "start_x=1             # essential
 gpu_mem=128           # at least, or maybe more if you wish" >> /boot/config.txt
