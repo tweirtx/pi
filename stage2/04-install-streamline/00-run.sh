6d13ff7340a569d4db0713695d2039fbc6322515
Travis Weir <tweirtx@gmail.com>
2020-07-21 21:02:29 -0500

    Fix more install bugs

--- stage2/04-install-streamline/00-run.sh
+++ stage2/04-install-streamline/00-run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cat /etc/apt/sources.list
 
+apt install gnupg -y
+
 curl https://bintray.com/user/downloadSubjectPublicKey?username=bintray | apt-key add -
 
 apt update
@@ -9,14 +11,9 @@ echo "Post update"
 
 # apt install gst-rpicamsrc rusty-engine gstreamer1.0-tools -y --allow-unauthenticated
 
-echo "start_x=1             # essential
-gpu_mem=128           # at least, or maybe more if you wish" >> /boot/config.txt
+echo "start_x=1" >> /boot/config.txt
+echo "gpu_mem=128" >> /boot/config.txt
 
-echo "[Unit]
-Description=Expose RPi Cam over RTSP
-After=network-online.target
-
-[Service]
-ExecStart=rusty-engine -d /dev/video0 -h 720 -w 1280 -i rpi" > /etc/systemd/system/rusty.service
+cp rusty.service /etc/systemd/system/rusty.service
 
 echo "Post service creation"
